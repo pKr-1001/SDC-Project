151507b0000cdb0445711701cb296ebcea31c735
park <minpark93@gmail.com>
2024-03-01 13:52:51 -0800

    working on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,14 @@ const Header = () => {
             <div className="announcement">
             <p className="a-text text-center w-full">BOGO 50% Off 
                 <a href="#" className="a-line px-0.5"> Craft Instant </a> 
-                Start a Blue Bottle Coffee <a href="#" className="a-line">Subscription</a> | 
+                Start a Blue Bottle Coffee 
+                <a href="#" className="a-line">Subscription</a> | 
                 Free Shipping on Orders Over $40
             </p> 
-        </div>
+            </div>
+            <div className="nav-bar w-full">
+                testing
+            </div>
       </div>
     )
 }
