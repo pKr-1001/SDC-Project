434246ac03cf97f0f5c241b5ab14aab85e1877b6
brandonphillip <drummaster2117@gmail.com>
2024-03-01 18:25:41 -0500

    working on footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"; // 3/1/24
+
+const Footer = () => {
+
+    return (
+        <>
+                <footer className="footer-data">
+                    
+                </footer>
+        </>
+    )
+}
+
+export default Footer;
