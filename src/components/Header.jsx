4e363cd1b9dc6d7b75fb72a4419b72a2bd104ef8
park <minpark93@gmail.com>
2024-03-01 14:28:06 -0800

    working on Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+// import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
 const Header = () => {
     return (
         <div className="header">
@@ -5,13 +7,20 @@ const Header = () => {
             <p className="a-text text-center w-full">BOGO 50% Off 
                 <a href="#" className="a-line px-0.5"> Craft Instant </a> 
                 Start a Blue Bottle Coffee 
-                <a href="#" className="a-line">Subscription</a> | 
+                <a href="#" className="a-line">{' '}Subscription</a> | 
                 Free Shipping on Orders Over $40
             </p> 
             </div>
-            <div className="nav-bar w-full">
-                testing
+        <nav className="nav-bar">
+            <div className="w-full flex flex-col">
+                <span role="button" className="nav-item">
+                    <svg className="textBlack">
+                    <rect width="20" height="1.33333"></rect>
+                    <rect y="5.33203" width="20" height="1.33333"></rect>
+                    </svg>
+                </span>
             </div>
+        </nav>
       </div>
     )
 }
