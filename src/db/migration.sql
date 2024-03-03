5479ab415faa044dd2c1372fe044a7e998ff43dc
Lance Tse <a.tse667@gmail.com>
2024-03-03 12:00:33 -0800

    split images into separate table, with mug_id REFERENCE mugs(mug_id)

--- src/db/migration.sql
+++ src/db/migration.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS mugs CASCADE;
+DROP TABLE IF EXISTS mug_pics CASCADE;
 
 CREATE TABLE mugs (
     mug_id SERIAL PRIMARY KEY,
@@ -6,7 +7,11 @@ CREATE TABLE mugs (
     mug_description_1 VARCHAR,
     mug_description_2 VARCHAR,
     mug_shipping VARCHAR,
-    mug_cost INTEGER,
+    mug_cost INTEGER
+);
+
+CREATE TABLE mug_pics (
+    mug_id INT REFERENCES mugs(mug_id),
     mug_pic_main VARCHAR,
     mug_pic_extra VARCHAR
 );
