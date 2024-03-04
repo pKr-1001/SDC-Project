52d0bc0fc58ca63fd8885d24bb09e82906932cc6
Humberto Palacios <palacioshumberto35@yahoo.com>
2024-03-04 09:32:27 -0800

    Main Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
 
   return (
     <>
         <Header />
+        <Main/>
     </>
   );
 }
