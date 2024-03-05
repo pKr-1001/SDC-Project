c386093fb80707d381f7767116c6c346ade55a23
Lance Tse <a.tse667@gmail.com>
2024-03-05 12:34:21 -0800

    added bottom description and pic of target page into database

--- src/db/migration.sql
+++ src/db/migration.sql
@@ -7,11 +7,19 @@ CREATE TABLE mugs (
     mug_description_1 VARCHAR,
     mug_description_2 VARCHAR,
     mug_shipping VARCHAR,
-    mug_cost INTEGER
+    mug_cost INTEGER,
+    mug_bottom_description_title VARCHAR,
+    mug_bottom_header_1 VARCHAR,
+    mug_bottom_description_1 VARCHAR,
+    mug_bottom_header_2 VARCHAR,
+    mug_bottom_description_2 VARCHAR,
+    mug_bottom_header_3 VARCHAR,
+    mug_bottom_description_3 VARCHAR
 );
 
 CREATE TABLE mug_pics (
     mug_id INT REFERENCES mugs(mug_id),
     mug_pic_main VARCHAR,
-    mug_pic_extra VARCHAR
+    mug_pic_extra VARCHAR,
+    mug_pic_extra_2 VARCHAR
 );
