36f3a3c50cf48db6d097d1e512e114f542489345
Lance Tse <a.tse667@gmail.com>
2024-03-11 14:42:36 -0700

    change relative path to images for imageContext

--- src/components/imageContext.jsx
+++ src/components/imageContext.jsx
@@ -3,11 +3,19 @@ import { createContext, useState } from 'react';
 const imageContext = createContext();
 
 const ImageProvider = ({children}) => {
+    // const [imageArr, setImage] = useState(
+    //     ['/src/assets/fellowMugMain.webp',
+    //      '/src/assets/fellowMugExtra.webp',
+    //      '/src/assets/fellowMugExtra2.webp'
+    //     ]);
+
     const [imageArr, setImage] = useState(
-        ['/src/assets/fellowMugMain.webp',
-         '/src/assets/fellowMugExtra.webp',
-         '/src/assets/fellowMugExtra2.webp'
-        ]);
+            [
+            'dist/assets/fellowMugMain-B42gLWKu.webp',
+            'dist/assets/fellowMugExtra-CiwDVw-L.webp',
+            'dist/assets/fellowMugExtra2-D79ScuSf.webp'
+            ]
+        );
 
         return (
             <imageContext.Provider value={imageArr}>
